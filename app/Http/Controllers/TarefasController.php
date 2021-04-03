658e32182988e5134b6177c823a6e190b6498d50
Sérgio Lima <sergiolimafg3@gmail.com>
2021-04-03 10:06:42 -0300

    Atualizado para Eloquente ORM.

--- app/Http/Controllers/TarefasController.php
+++ app/Http/Controllers/TarefasController.php
@@ -3,13 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB; // IMPORTANDO DB
+//use Illuminate\Support\Facades\DB; // Usando QueryBuilder
+use App\Tarefa;
 
 class TarefasController extends Controller
 {
 
     public function list(){
-        $list = DB::select('SELECT * FROM tarefas');
+
+        //$list = DB::select('SELECT * FROM tarefas'); QueryBuilder
+
+        $list = Tarefa::all();// Eloquent ORM
 
         return view('tarefas.list',[
             'list' => $list
@@ -25,24 +29,34 @@ class TarefasController extends Controller
                 'titulo' => ['required', 'string']
             ]);
 
-            $titulo = $request->input('titulo');
 
-            DB::insert('INSERT INTO tarefas (titulo) VALUES (:titulo)', [
+            $titulo = $request->input('titulo');
+                // QueryBuilder
+            /*DB::insert('INSERT INTO tarefas (titulo) VALUES (:titulo)', [
                 'titulo'=>$titulo
-            ]);
+            ]);*/
+
+            //Eloquent ORM
+            $t = new Tarefa;
+            $t->titulo = $titulo;
+            $t->save();
 
             return redirect()->route('tarefas.list');
 
 
     }
     public function edit($id){
-        $data = DB::select('SELECT * FROM tarefas WHERE id = :id', [
+            // QueryBuilder
+        /*$data = DB::select('SELECT * FROM tarefas WHERE id = :id', [
             'id'=> $id
-        ]);
+        ]);*/
 
-        if(count($data) > 0) {
+            //Eloquent ORM
+        $data = Tarefa::find($id);
+
+        if($data) {
             return view('tarefas.edit', [
-                'data' => $data[0]
+                'data' => $data
             ]);
         }else {
             return redirect()->route('tarefas.list');
@@ -56,25 +70,43 @@ class TarefasController extends Controller
         ]);
 
             $titulo = $request->input('titulo');
-
-                DB::update('UPDATE tarefas SET titulo = :titulo WHERE id = :id', [
+                    // QueryBuilder
+                /*DB::update('UPDATE tarefas SET titulo = :titulo WHERE id = :id', [
                     'id' => $id,
                     'titulo' => $titulo
-                ]);
+                ]);*/
+
+                    //Eloquent ORM
+                $t = Tarefa::find($id);
+                $t->titulo = $titulo;
+                $t->save();
 
             return redirect()->route('tarefas.list');
 
     }
     public function del($id){
-        DB::delete('DELETE FROM tarefas WHERE id = :id', [
+             // QueryBuilder
+        /*DB::delete('DELETE FROM tarefas WHERE id = :id', [
             'id' => $id
-        ]);
+        ]);*/
+
+             //Eloquent ORM
+        Tarefa::find($id)->delete();
+
         return redirect()->route('tarefas.list');
     }
     public function done($id){
-        DB::update('UPDATE tarefas SET resolvido = 1 - resolvido WHERE id = :id', [
+              // QueryBuilder
+        /*DB::update('UPDATE tarefas SET resolvido = 1 - resolvido WHERE id = :id', [
                 'id' => $id
-            ]);
+            ]);*/
+
+                //Eloquent ORM ?
+            $t = Tarefa::find($id);
+            if($t){
+                $t->resolvido = 1 - $t->resolvido;
+                $t->save();
+            }
 
 
         return redirect()->route('tarefas.list');
